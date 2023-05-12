dbe570543843ba7370d042f262000c8cd1936f47
OV3rSimplified <twinkelbuggs@gmail.com>
2023-05-12 16:55:14 +0200

    cc
    cc

--- src/logic.js
+++ src/logic.js
@@ -1,36 +1,55 @@
-var BackgroundSetting = BackgroundSetting || 0
-const themeToggle = document.getElementById('theme-toggle');
-const WikiItems = document.getElementsByClassName("WikiItems");
+const WikiItem = document.getElementById('WikiItems');
 
-function WindowMode(mode) {
-    const body = document.body
-
-    if (mode==="dark") {
-        body.style.backgroundColor = "white"
-        document.getElementById("ldicon").setAttribute("src", "img/brightness.png");
-    }
-    else if (mode === "light") {
-        body.style.backgroundColor = "#111"
-        document.getElementById("ldicon").setAttribute("src", "img/night-mode.png");
+function convertMarkdownToHtml(text) {
+  // Überschriften umsetzen
+  const headings = text.replace(/^(#+)\s*(.*?)\s*#*$/gm, (match, p1, p2) => {
+    const level = p1.length;
+    if (level === 1) {
+      return `<h${level} id='wiki_titles1'>${p2}</h${level}> <hr id="titlelined">`;
+    } else if ( level == 2) {
+      return `<h${level} id='wiki_titles2'>${p2}</h${level}>`;
     };
+  });
+
+  // Fettdruck umsetzen
+  const bold = headings.replace(/\*\*(.*?)\*\*/g, "<strong id='wiki_bold'>$1</strong>");
+
+  // Kursivschrift umsetzen
+  const italic = bold.replace(/\*(.*?)\*/g, "<em id='wiki_italic'>$1</em>");
+
+  console.log(italic)
+  document.getElementById('main_s').innerHTML = italic
 }
 
-themeToggle.addEventListener('click', () => {
-    let mode = localStorage.getItem("BgSetting");
-    console.log(mode)
-    if (mode === null) { localStorage.setItem("BgSetting", "light")}
+function ReadPage(page, item) {
+  fetch('pages/'+page+'/'+ item)
+    .then(response => response.text())
+    .then(data => {
+      console.log(data); // Hier wird die Antwort der PHP-Datei ausgegeben
+      convertMarkdownToHtml(data)
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
 
-    if (mode === "light") {
-        WindowMode("dark")
-        localStorage.setItem("BgSetting", "dark")
-    } else if (mode == "dark" ) {
-        WindowMode("light")
-        localStorage.setItem("BgSetting", "light")
+function InitDocsPage(page) {
+    const wikiContainer = document.getElementById("WikiContainer");
+    const parent = document.querySelector('#main_s');
+
+    if (wikiContainer !== null) {
+      wikiContainer.style.display = "none";
+      // Textbereich erstellen
+      const markdownText = document.createElement("div");
+      markdownText.id = "markdown-text";
+      markdownText.style.fontFamily = "Montserrat";
+      markdownText.style.fontSize = "20px";
+      parent.appendChild(markdownText)
+      console.log(parent)
     }
 
-
-
-});
-
-WikiItems.addEventListener('click', () => {
+    ReadPage('hexchars', 'home.php' )
+}
+WikiItem.addEventListener('click', () => {
+  InitDocsPage('hexchars')
 });
